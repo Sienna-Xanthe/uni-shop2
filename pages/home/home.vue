ab1b374e4dabdbb51a7dcde294c0165ef6122011
Sienna-Xanthe <3043015301@qq.com>
2022-02-22 22:30:46 +0800

    完成了搜索页面的开发

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,5 +1,10 @@
 <template>
   <view>
+    <!-- 搜索组件 -->
+    <view class="search-box">
+      <my-search @click="gotoSearch"></my-search>
+    </view>
+    
     <!-- 轮播图的区域 -->
     <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" circular="true">
       <swiper-item v-for="(item, i) in swiperList" :key="i">
@@ -106,6 +111,11 @@
        
        this.floorList = res.message
        // uni.$showMsg('数据请求成功！')
+      },
+      gotoSearch() {
+        uni.navigateTo({
+          url: '/subpkg/search/search'
+        })
       }
     }
   }
@@ -142,5 +152,10 @@ swiper {
 .floor-img-box{
   display: flex;
   padding-left: 10rpx;
+}
+.search-box {
+  position: sticky;
+  top: 0;
+  z-index: 999;
 }
 </style>
